fbdc3a7533734a0855bee4bf42062bf5ab51282a
Xandra <xandrabest@gmail.com>
2019-04-18 19:21:55 -0500

    add amazon summary data constants

--- Imports/Utilities/dataProcessing.py
+++ Imports/Utilities/dataProcessing.py
@@ -1,20 +1,10 @@
+from .constants import *
 from .fileManagement import *
 from .output import *
 
 def processReports():
 	filePaths = collectFilePaths()
-
-	summaryData = collectSummaryData()
-	# beginningBalanceSubtotal = promptForManualDataEntry('Beginning Balance Subtotal')
-	# ordersSubtotal = promptForManualDataEntry('Orders Subtotal')
-	# refundsSubtotal = promptForManualDataEntry('Refunds Subtotal')
-	# shippingServicesSubtotal = promptForManualDataEntry('Shipping Services Subtotal')
-	# totalBalance = promptForManualDataEntry('Total Balance')
-	# unavailableBalance = promptForManualDataEntry('Unavailable Balance')
-	# transferAmount = promptForManualDataEntry('Transfer Amount')
-
-	print(filePaths)
-	print(summaryData)
+	amazonSummaryData = collectAmazonSummaryData()
 
 
 def collectFilePaths():
@@ -24,8 +14,8 @@ def collectFilePaths():
 	return filePaths
 
 
-def collectSummaryData():
+def collectAmazonSummaryData():
 	output('Please enter the following values from the Amazon summary:')
-	summaryData = {}
-	summaryData['Beginning Balance Subtotal'] = float(prompt('Beginning Balance Subtotal','$').replace(',',''))
-	return summaryData
+	amazonSummaryData = {}
+	amazonSummaryData['Beginning Balance Subtotal'] = float(prompt('Beginning Balance Subtotal','$').replace(',',''))
+	return amazonSummaryData
