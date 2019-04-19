b08a44bb988e37c7e385ca4bf02d32b8e1f85c13
Xandra <xandrabest@gmail.com>
2019-04-19 01:49:04 -0500

    reorder and reformat excel file

--- Imports/Utilities/excel.py
+++ Imports/Utilities/excel.py
@@ -17,6 +17,23 @@ def loadSheet(workBook, fileType):
 	return sheet
 
 
+def setCellValue(sheet, row, column, value):
+	sheet.cell(row = row, column = column).value = value;
+
+
+def getCellValue(sheet, row, column):
+	return sheet.cell(row = row, column = column).value
+
+
+def getCellString(sheet, row, column):
+	return str(getCellValue(sheet, row, column)).upper()
+
+
+def getCellDateString(sheet, row, column):
+	dateString = getCellString(sheet, row, column)
+	return datetime.strptime(dateString[0 : dateString.index(',') + 6], '%b %d, %Y')
+
+
 def processQuickBooksReport(filePath):
 	return {} # TODO ACCOUNTED
 
@@ -29,18 +46,35 @@ def processAmazonReport(filePath, quickBooksRecords):
 	nonOrders = []
 
 	for i in range(8 + 1, sheet.max_row + 1): # TODO store start value in constant
-		recordType = readCellString(sheet, i, 3) # TODO store column index in constant
+		recordType = getCellString(sheet, i, 3) # TODO store column index in constant
 		if recordType == 'ORDER': #TODO constant
 			pass # TODO ACCOUNTED store in orders
 		else:
-			nonOrders.append(AmazonNonOrderRecord(i, readCellDateString(sheet, i, 1), recordType))
-	
-	for i in sorted(nonOrders):
-		print(i.summarize())
+			nonOrders.append(AmazonNonOrderRecord(i, getCellDateString(sheet, i, 1), recordType))
 
-def readCellString(sheet, row, column):
-	return str(sheet.cell(row = row, column = column).value).upper()
+	modifyAmazonReport(sheet, orders, nonOrders)
+	workBook.save('happylilfile.xlsx')
 
-def readCellDateString(sheet, row, column):
-	dateString = readCellString(sheet, row, column)
-	return datetime.strptime(dateString[0 : dateString.index(',') + 6], '%b %d, %Y')
+
+def modifyAmazonReport(sheet, orders, nonOrders):
+	copyNonOrders(sheet, nonOrders)
+	removeNonOrders(sheet, nonOrders)
+	removeHeaderRows(sheet)
+
+
+def copyNonOrders(sheet, nonOrders):
+	row = sheet.max_row + 4 # TODO constant stuff
+	for nonOrder in sorted(nonOrders):
+		for column in range(1, 25 + 1): # TODO constant stuff
+			setCellValue(sheet, row, column, getCellValue(sheet, nonOrder.getRow(), column))
+		row = row + 1
+		
+
+def removeNonOrders(sheet, nonOrders):
+	for nonOrder in sorted(nonOrders, key = lambda x: x.row, reverse = True):
+		sheet.delete_rows(nonOrder.row)
+
+
+def removeHeaderRows(sheet):
+	sheet.delete_rows(1, 7)
+
